a2b687f1fb7881cd689ded22be603566652945e7
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-02-23 10:51:52 +0000

    Update main.js

--- main.js
+++ main.js
@@ -13,11 +13,24 @@ sendNotification.addEventListener("click", function(){
       const myFirstNotif = new Notification("Example Notification", {
         body: "hey dude",
         data: {Asher: "Perfection"},
-        icon: "me.png"
+        icon: "me.png",
+        // tag: "Warning Message"
       })
       myFirstNotif.addEventListener("close", e =>{
         // alert("well done!!!!!!!")
       })
     }
   })
+})
+
+let secondNotif;
+document.addEventListener("visibilitychange", function(){
+  if (document.visibilityState === "hidden") {
+    secondNotif = new Notification("You left??", {
+      boby: "Hope you come back soon",
+      tag: "Get Back"
+    })
+  } else {
+    // secondNotif.close()
+  }
 })
